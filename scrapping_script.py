3a037682de8070a061fee7b0ffd2b56f7a3a07b4
--LG--DKX <github.8ee8v@simplelogin.co>
2023-02-20 00:08:43 +0100

    Modifying everything

--- scrapping_script.py
+++ scrapping_script.py
@@ -1,43 +1,41 @@
 """Scrapping script to compare game price between official website such as steam and Instant Gaming.
 """
 
-###############################################
-# Import BeautifulSoup in order to scrap data #
-###############################################
+####################################################
+# Import BeautifulSoup in order to fetch HTML code #
+####################################################
 from bs4 import BeautifulSoup
-
 
 #########################################################
 # Import requests package in order to make web requests #
 #########################################################
 import requests
 
-# Website URL
-url = "https://www.instant-gaming.com/fr/jeux/steam/" #pylint: disable=all
+#########################################################
+# Import re package in order to use regular expressions #
+#########################################################
+# import re
 
-# User-Agent header to simulate a web browser
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
-    (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-}
+# Loop through pages 1 to 134
+for page in range(1, 3):
+    # Base URL that will change at every turn of the loop
+    url = f"https://www.instant-gaming.com/fr/rechercher/?type%5B0%5D=steam&page={page}" #pylint: disable=all
 
-# Fetch the page
-result = requests.get(url, headers=headers)
-doc = BeautifulSoup(result.text, "html.parser")
+    # User-Agent header to simulate a web browser
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+        (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
 
-# Fetch the game's title and price
-title = doc.find("h1")
-price = doc.find("div", class_="total")
+    # Fetch the page
+    result = requests.get(url, headers=headers)
+    doc = BeautifulSoup(result.text, "html.parser")
 
-# Print the game's title without html code
-for title in title:
-    print(title.strip())
+    links = doc.find_all("a", class_="cover")
+    titles = doc.find_all("span", class_="title")
 
-# Print the game's price without html code and by adding the TVA (The real price in short)
-for price in price:
-    # Remove the html code and the € symbol
-    price = float(price.strip().replace("€", ""))
-    # Add the 20 % TVA Tax
-    real_price = price * 1.20
-    # Print the price
-    print("Price : {:.2f} €".format(real_price))
+    for link in links:
+        print(link.get("href"))
+
+    for title in titles:
+        print(title.text)
