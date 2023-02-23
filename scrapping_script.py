757f3f7d0559d126785bd3448992e3240894f947
--LG--DKX <github.8ee8v@simplelogin.co>
2023-02-23 15:28:58 +0100

    Changing things

--- scrapping_script.py
+++ scrapping_script.py
@@ -1,4 +1,4 @@
-"""Scrapping script to compare game price between official website such as steam and Instant Gaming.
+"""Scrapping script to get all the prices on Instant Gaming with TVA added
 """
 
 ####################################################
@@ -16,70 +16,116 @@ import requests
 ##################################################################
 import pandas as pd
 
-# User-Agent header to simulate a web browser
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
-    (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-}
-    
-def fetch_data():
-    # Create empty lists to store the scraped data
-    links = []
-    titles = []
-    prices = []
+class InstantGamingScraper:
+    """Class to scrap prices from Instant Gaming
+    """
 
-    # Number of titles we want to skip at the beginning
-    skip = 4
-    
-    # Loop through pages 1 to 134
-    for page in range(1, 2):
-        # Base URL that will change at every turn of the loop
-        url = f"https://www.instant-gaming.com/fr/rechercher/?type%5B0%5D=steam&page={page}" #pylint: disable=all    
-        
-        # Fetch the page
-        result = requests.get(url, headers=headers)
-        doc = BeautifulSoup(result.text, "html.parser")
-        
-        # Fetch the games pages's link
-        links += [link.get("href") for link in doc.find_all("a", class_="cover")]
+    def __init__(self, num_pages: int):
+        # Set the numbers of pages
+        self.num_pages = num_pages
+        # User-Agent header to simulate a web browser
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+            (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        }
 
-        page_titles = [title.text for title in doc.find_all("span", class_="title")]
-        titles += page_titles[skip:]
+    def fetch_data(self):
+        """This function fetch data
+        """
+        # Create empty lists to store the scraped data
+        links = []
+        titles = []
+        prices = []
+        final_prices = []
 
-        #Fetch the games's price
-        information_divs = doc.find_all("div", class_="information")[1:]
-        for div in information_divs:
-            price_div = div.find("div", class_="price")
-            if price_div:
-                price = float(price_div.text.replace(',', '.').split('€')[0])
-            else:
-                price = "No price"
-            if price == "No price":
-                print(div)
-            prices.append(price)
+        # Number of titles we want to skip at the beginning
+        skip = 4
 
-        #Fetch the games's price
-        # prices += [float(price.text.replace(',', '.').split('€')[0]) for price in doc.find_all("div", class_="price")[1:]]
+        # Loop through pages 1 to 134
+        for page in range(1, self.num_pages+1):
+            # Base URL that will change at every turn of the loop
+            url = f"https://www.instant-gaming.com/fr/rechercher/?type%5B0%5D=steam&page={page}"
 
-        # print(f"Page {page}: titles={len(titles)}, prices={len(prices)}, links={len(links)}")
+            # Fetch the page
+            result = requests.get(url, headers=self.headers, timeout=60)
+            doc = BeautifulSoup(result.text, "html.parser")
 
-    # # Add 20% to the prices
-    # if price != "No price"
-    #     prices = [round(price * 1.2, 2) for price in prices]
+            # Fetch the games pages's link
+            links += [link.get("href") for link in doc.find_all("a", class_="cover")]
 
-    return titles, prices, links
-           
-def save_to_csv(titles, prices, links):
-    # Create a pandas DataFrame from the scraped data
-    data = {'Title': titles, 'Price': prices, 'Link': links}
-    df = pd.DataFrame(data)
-    
-    # Save the DataFrame to a CSV file
-    df.to_csv('game_prices.csv', index=False)
-    print('Data saved to game_prices.csv')
+            #Fetch the games's title
+            page_titles = [title.text for title in doc.find_all("span", class_="title")]
+            titles += page_titles[skip:]
 
-def execute():
-    titles, prices, links, = fetch_data()
-    save_to_csv(titles, prices, links,)
+            #Fetch the games's price
+            information_divs = doc.find_all("div", class_="information")
+            for i, div in enumerate(information_divs):
+                if i == len(information_divs) - 1:
+                    continue
+                # information_div = div.find("div", class_="information")
+                price_div = div.find("div", class_="price")
+                if price_div:
+                    price = float(price_div.text.replace(',', '.').split('€')[0])
+                    final_price = round(price * 1.2, 2)
+                else:
+                    price = "No price"
+                    final_price = "No price"
+                prices.append(price)
+                final_prices.append(final_price)
 
-execute()
+        # # Add 20% to the prices
+        # if price != "No price"
+        #     prices = [round(price * 1.2, 2) for price in prices]
+
+        return titles, prices, final_prices,     links
+
+    def save_to_csv(self, titles, prices, final_prices, links):
+        """This function send the data to a csv file
+        """
+        # Create a pandas DataFrame from the scraped data
+        data = {'Title': titles, 'Price': prices, 'Final Price': final_prices, 'Link': links}
+        df = pd.DataFrame(data)
+
+        # Save the DataFrame to a CSV file
+        df.to_csv('game_prices.csv', index=False)
+        print('Données sauvegardés dans "game_prices.csv"')
+
+    def search(self):
+        """This function search for a game by name
+        """
+
+        # Ask the user for a game name
+        game_name = input("Entrez le nom du jeu à chercher : ")
+
+        # Fetch the data
+        titles, prices, final_prices, links = self.fetch_data()
+
+        # Create a list of matching games
+        matching_games = []
+        for i, title in enumerate(titles):
+            if game_name.lower() in title.lower():
+                matching_games.append((title, prices[i], final_prices[i], links[i]))
+
+        # Display the matching games
+        if len(matching_games) == 0:
+            print("Aucun jeu correspondant trouvé.")
+        else:
+            print(f"Voici les {len(matching_games)} jeux correspondants :")
+            for game in matching_games:
+                print(f"Titre : {game[0]}")
+                print(f"Prix : {game[1]}€")
+                print(f"Prix final : {game[2]}€")
+                print(f"Lien : {game[3]}")
+                print()
+
+    def execute(self):
+        """This function execute all other functions
+        """
+        titles, prices, final_prices, links, = self.fetch_data()
+        self.save_to_csv(titles, prices, final_prices, links,)
+
+# Create an instance of the InstantGamingScraper class
+scraper = InstantGamingScraper(num_pages=int(input("Combien de pages doivent êtres analysés ? : ")))
+
+# Call the execute() method on the instance to start the scraping process
+scraper.execute()
